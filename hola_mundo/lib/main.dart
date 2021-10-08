1c7f74c391bc60369d6ee9eb0b5a42bb14b7cd1f
Sebastian Castañeda <sebcastanedarom@unal.edu.co>
2021-10-08 15:23:22 -0500

    hola mundo

---
+++ hola_mundo/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Desarrollo de App movil',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Desarrollo de App movil'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text(widget.title)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hola Mundo',
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 50),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
